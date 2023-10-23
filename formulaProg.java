ca26830bdb8d651a3ef02557f3cdcab01ee32683
SriIndira <148343167+SriIndira@users.noreply.github.com>
2023-10-23 11:01:51 +0530

    changes added

--- formulaProg.java
+++ formulaProg.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 class formulaProg {
     public static void main(String[] args){
-        System.out.println(" Enter Config Type >>>>  ");
+        System.out.println(" Enter Config Type >>>>n 1.FixedBias, 2.Emitter Bias  ");
         formulaProg exe = new formulaProg();
         Scanner input = new Scanner(System.in);
-        String type = input.nextLine();
+        int type = input.nextInt();
         switch(type){
-            case "Fixed-bias":
+            case 1:
                 exe.fixedBias();
                 break;
+            case 2:
+                exe.emitterbias();
+                break;
+
         }
     }
 
@@ -32,6 +36,30 @@ class formulaProg {
         int Vce = Vcc-(Ic*Rc);
         System.out.println("Value of Ib...."+Ib);
         System.out.println("Value of Ic...."+Ic);
+        System.out.println("Value of Vce...."+Vce);
+    }
+    private void emitterbias(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Values For....");
+        System.out.println("Enter Voltage CC >>> ");
+        int Vcc = input.nextInt();
+        System.out.println("Enter Voltage BE >>> ");
+        int Vbe = input.nextInt();
+        System.out.println("Enter Rb>>> ");
+        int Rb = input.nextInt();
+        System.out.println("Enter Re>>> ");
+        int Re = input.nextInt();
+        System.out.println("Enter Rc>>> ");
+        int Rc = input.nextInt();
+        System.out.println("Enter Beta >>> ");
+        int Beta = input.nextInt();
+        int Ib = (Vcc-Vbe)/(Rb+(Beta+1)*Re);
+        int Ic = (Beta+1)*Ib;
+        int Ri = (Beta+1)*Re;
+        int Vce = Vcc-(Ic*(Rc+Re));
+        System.out.println("Value of Ib...."+Ib);
+        System.out.println("Value of Ic...."+Ic);
+        System.out.println("Value of Ri...."+Ri);
         System.out.println("Value of Vce...."+Vce);
     }
 }
